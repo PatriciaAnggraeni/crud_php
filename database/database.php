c2e712f9deb817a770593bc4b9a8c30f2b412f71
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-03-07 22:14:28 +0700

    membuat koneksi ke database

---
+++ database/database.php
@@ -0,0 +1,40 @@
+<?php
+
+// membuat konfirgurasi koneksi ke database menggunakan konsep oop
+
+class Database {
+
+    // membuat proprerti class
+    private $dbHost = "127.0.0.1";
+    private $dbUser = "AnggaSusilo";
+    private $dbPassword = "Anggara27042022";
+    private $dbName = "crud_php";
+
+    // membuat konstruktor class
+    public function __construct() {
+        
+        // memanggil fungsi connect di dalam konstruktor
+        $this->connect();
+
+    }
+
+    // membuat method untuk koneksi ke database
+    public function connect() {
+
+        // buat variabel untuk menampung hasil koneksi database
+        $mysqli = new mysqli($this->dbHost, $this->dbUser, $this->dbPassword, $this->dbName);
+
+        // buat kondisi jika koneksi ke database berhasil
+        if ( !$mysqli->connect_error ) {
+            echo "koneksi ke database berhasil";
+        } else { 
+            echo "koneksi ke database gagal [" . $mysqli->connect_error . "]";
+        }
+
+        // kembalikan nilai dari koneksi
+        return $mysqli;
+    }
+}
+
+// membuat objek database
+$connectToDb = new Database();
